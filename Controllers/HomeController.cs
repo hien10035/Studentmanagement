ba2e975cc8368a447e1fbc32c2414b5484c02da4
randow3103 <hien.207ct10035@vanlanguni.vn>
2021-11-09 09:29:53 +0700

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace StudentsManagement.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hello mọi người";
 
             return View();
         }
